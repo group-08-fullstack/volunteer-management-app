e20b395f1d7fd28613e0cbeffed16d9d1a5db667
group-08-fullstack <ttorta005@gmail.com>
2025-07-02 18:49:54 -0500

    Update VolunteerMatch.jsx

--- src/components/VolunteerMatch.jsx
+++ src/components/VolunteerMatch.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// Simulated notification function
+// notification
 function sendNotification(email, message) {
   alert(`Notification to ${email}: ${message}`);
 }
